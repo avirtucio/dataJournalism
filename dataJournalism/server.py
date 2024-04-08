d344051562355e6fce28f148a8270df3fb58bc9e
avirtucio <160163737+avirtucio@users.noreply.github.com>
2024-04-08 14:26:02 -0400

    Add files via upload

---
+++ dataJournalism/server.py
@@ -0,0 +1,16 @@
+# flask --app data_server run
+from flask import Flask
+from flask import render_template
+from flask import request
+import json
+import math
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def about():
+
+    return render_template(about.html)
+
+
+app.run(debug=True)
