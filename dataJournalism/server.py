96d81fbd02a9cdca0d0ce12d354c8365ec181d30
avirtucio <arlo.virtucio25@trinityschoolnyc.org>
2024-04-14 16:14:01 -0400

    Created dictionary of % of pop thats disadv in each district

--- dataJournalism/server.py
+++ dataJournalism/server.py
@@ -13,8 +13,23 @@ def about():
     data = json.load(f)
     f.close()
     
-    #print(data)
+    percentDict = {}
+    for district in data:
+        disadv2023 = data[district]["Econ Disadv"]["2023"]["NumTested"]
+        notdisadv2023 = data[district]["Not Econ Disadv"]["2023"]["NumTested"]
+        total = disadv2023 + notdisadv2023
+        percentDict[district] = int((disadv2023/total)*100)
 
+    lowestperc, highestperc = 101,0
+    for district in percentDict:
+        if (percentDict[district] > highestperc):
+            highestperc = percentDict[district]
+        if (percentDict[district] < lowestperc):
+            lowestperc = percentDict[district]
+
+    print(percentDict)
+    print("Range is "+str(lowestperc)+"% to "+str(highestperc)+"%")
+    
     return render_template('about.html')
 
 
